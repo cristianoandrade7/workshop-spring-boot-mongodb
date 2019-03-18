fba5fc2f66eaf290adb40ecfd7c376daf23c62ee
cristianoandrade7 <cristiano@7waves.me>
2019-03-18 16:49:13 -0300

    insercao com POSt

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotfoundException;
 
@@ -24,6 +25,16 @@ public class UserService {
 		if (user == null) {
 			throw new ObjectNotfoundException("Objeto não Encontrado");
 		}
-				return user;		
+		return user;		
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto){
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
 }
+}
+
