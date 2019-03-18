33d8a826b75275eb7db4b232f76fe651527bf130
cristianoandrade7 <cristiano@7waves.me>
2019-03-18 16:11:58 -0300

    Obtendo um usuario por ID

---
+++ src/main/java/com/nelioalves/workshopmongo/services/exception/ObjectNotfoundException.java
@@ -0,0 +1,9 @@
+package com.nelioalves.workshopmongo.services.exception;
+
+public class ObjectNotfoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+		
+	public ObjectNotfoundException(String msg) {
+		super(msg);
+	}
+}
